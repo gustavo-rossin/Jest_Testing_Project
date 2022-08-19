336fd107b0ccd4779c46057acf30800acbbbf7db
gustavo-rossin <gustavo.rossin@hotmail.com>
2022-08-19 15:44:14 -0300

    chore: finished task 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,20 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const feedback = () => 'Eita pessoa boa!';
+const createStudent = (student) => {
+  let nameFeedback = {
+    name: student,
+    feedback,
+  };
+  return nameFeedback;
+};
+
+const estudante = createStudent('Leandrão, o Lobo Solitário');
+console.log(`1º ${estudante.name}`);
+console.log(estudante.feedback());
+console.log(typeof estudante);
+console.log(typeof estudante.name);
+console.log(typeof estudante.feedback);
 
 module.exports = createStudent;
